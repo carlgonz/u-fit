4a70d6cb119247431b3513d8c889a55b7bfe893a
cgonzalez <cgonzalez@alges.cl>
2014-04-16 20:14:02 -0300

    More options to test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import numpy as np
-from mayavi.mlab import surf, show, figure, axes
+from mayavi.mlab import surf, show, figure, axes, points3d
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
@@ -22,34 +22,57 @@ def list_datafiles(path):
 
 def plot_3d_mayavi(X, Y, Z):
     f = figure(bgcolor=(1, 1, 1))
-    surf(X.T, Y.T, Z, figure=f)
+
+    surf(X.T, Y.T, Z, figure=f, warp_scale='auto')
     axes(xlabel='N Samples', ylabel='Sample', zlabel='Gradient',
-         nb_labels=5, line_width=1)
+         z_axis_visibility=False, nb_labels=10, line_width=1)
 
     show()
+
 
 def plot_3d_mplot(X, Y, Z, title=""):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.plot_wireframe(X.T, Y.T, Z, rstride=1, cstride=1, linewidth=0.5, antialiased=True)
+    ax.plot_surface(X.T, Y.T, Z.T, rstride=1, cstride=1, linewidth=0.5, antialiased=True)
     plt.xlabel("N Samples")
     plt.ylabel("Sample")
     plt.title(title)
     # ax.zlabel("Gradient")
     plt.show()
 
-def main():
-    test = 11
+
+def main(test=1, plot=True, save=False):
     test_list = list_datafiles("data/")
     data = np.loadtxt(test_list[test], delimiter=',', skiprows=1, usecols=(0, 1, 2))
-    slices = list(range(5, len(data[:, 0]), 1))
+    slices = list(range(10, len(data[:, 0]), 1))
     reg = Regression(data[:, 1], data[:, 2])
 
     Z = reg.analyze(slices)
     X, Y = np.meshgrid(slices, data[:, 0])
-    # plot_3d_mayavi(X, Y, Z)
-    plot_3d_mplot(X, Y, Z, title=test_list[test])
+    M = np.vstack([X.reshape((1, -1)), Y.reshape((1, -1)), Z.reshape((1, -1))])
 
+    if save:
+        filename = test_list[test].replace('data', 'out')
+        np.savetxt(filename, M.T, delimiter=',')
+        print("Saving to {0}".format(filename))
+
+    if plot:
+        plot_3d_mayavi(X, Y, Z)
+        # plot_3d_mplot(X, Y, Z, title=test_list[test])
+
+
+def test_all():
+    total = len(list_datafiles("data/"))
+    for i in range(total):
+        main(test=i, plot=False, save=True)
 
 if __name__ == "__main__":
-    main()
+    try:
+        if sys.argv[1] == "all":
+            test_all()
+        else:
+            main(int(sys.argv[1]))
+
+    except Exception as e:
+        print(e)
+        exit(0)
