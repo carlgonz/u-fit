bbac5f89c2d1b5aaa3be1bc059e10d38fc807336
cgonzalez <cgonzalez@alges.cl>
2014-05-04 23:56:41 -0400

    Updating install script

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from distutils.core import setup
 
 setup(
-    name='u-regression',
-    version='0.0.2',
-    url='https://github.com/carlgonz/u-regression',
+    name='u-fit',
+    version='0.0.3',
+    url='https://github.com/carlgonz/u-fit',
     license='MIT',
     author='Carlos Gonzalez',
     author_email='cgonzalez@alges.cl',
     description='A software fot automated curve fitting',
 
     packages=['src/python/forms', 'src/python/modules', 'src/python', ''],
-    scripts=['u-regression.py'],
+    scripts=['u-fit.py'],
     install_requires=["numpy", "scipy", "pygal"],
     data_files=[('src/css', ['src/css/base-new.css']),
                 ('src/js', ['src/js/pygal-tooltips.js', 'src/js/svg.jquery.js'])]
